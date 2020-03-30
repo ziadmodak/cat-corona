4d439b2d0cfd0539d55228d77082935b601712fd
Ziad Modak <46166766+ziadmodak@users.noreply.github.com>
2020-03-30 15:21:47 +0200

    Replace head with sed for macOS compatibility

--- corona.sh
+++ corona.sh
@@ -2,5 +2,5 @@
 
 COUNTRYCODE=$1
 
-curl --silent https://corona-stats.online/$COUNTRYCODE?source=2 | head -n -9 > ~/.cache/corona
+curl --silent https://corona-stats.online/$COUNTRYCODE?source=2 | sed -e :a -e '$d;N;2,10ba' -e 'P;D' > ~/.cache/corona
 cat ~/.cache/corona
